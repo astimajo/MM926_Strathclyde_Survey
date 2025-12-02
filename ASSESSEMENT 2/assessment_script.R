9e94dba16261436d3a2f6855090c70e9ec2c3a79
Angelo Timajo <64699717+astimajo@users.noreply.github.com>
2025-12-02 09:17:23 +0800

    Update assessment_script.R

--- ASSESSEMENT 2/assessment_script.R
+++ ASSESSEMENT 2/assessment_script.R
@@ -40,3 +40,81 @@ CronbachAlpha(data[, 6:20], cond = TRUE, conf.level = 0.95, na.rm = TRUE)
 ## from participants; (ii) elicited a strong negative response from participants;
 ## (iii) divided the opinion of participants.
 
+# Note: Code LIKERTS as factors first.
+# strongly disagree (1), disagree (2), neither agree nor disagree (3); agree (4), strongly
+# agree (5)
+
+# Performance, Social, Mental Health, Awareness, Connected, Resources, Expression,
+# Community, Networking, Interactions, Time Wasted, Concentration, Negative Content,
+# Data Security, Inadequate
+
+data$Performance <- factor(data$Performance, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Social <- factor(data$Social, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Mental_Health <- factor(data$Mental_Health, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Awareness <- factor(data$Awareness, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Connected <- factor(data$Connected, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Resources <- factor(data$Resources, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Expression <- factor(data$Expression, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Community <- factor(data$Community, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Networking <- factor(data$Networking, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Interactions <- factor(data$Interactions, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Time_Wasted <- factor(data$Time_Wasted, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Concentration <- factor(data$Concentration, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Negative_Content <- factor(data$Negative_Content, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Data_Security <- factor(data$Data_Security, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+data$Inadequate <- factor(data$Inadequate, levels = 1:5, labels = c("strongly disagree", "disagree", "neither agree nor disagree", "agree", "strongly agree"))
+
+# Visualizations
+library(likert)
+
+results <- likert(data[,6:20])
+
+plot(results, type="bar")
+plot(results, type="heat")
+plot(results, type="density")
+
+# Interpretation:
+
+# Strong positive responses can be seen for "Connected" (81.7% A+SA) and 
+# "Interactions" (73.6% A+SA). Students mostly strongly believe that Social 
+# media helps them stay connected with their family and friends and furthermore, 
+# they strongly disagreed that Social media reduced face-to-face interactions 
+# which is a positive response when taking the question to its context. 
+
+# Strong negative responses can be seen for "Time Wasted" (60.7% A+SA) and 
+# "Data Security" (58.5% A+SA) which shows that students feel that Social media
+# is a waste of their time and that they are concerned with their security based
+# on how they agreed on the negatively written questions.
+
+# Respondents were most divided on the "Concentration", "Inadequate" and 
+# "Mental Health". This means that students were not completely in agreement
+# whether it affects their concentration as some people may prefer to use
+# Social media as a positive reinforcement tool. As for inadequate some 
+# people use Social media as a means to watch entertainment so it does not
+# really apply to everyone and for Mental Health this is quite a divided topic
+# as some people use Social Media as a means to cope with their loneliness 
+# so this can be seen as an escape or they may perceive this as something they
+# need for their mental health.
+
+
+## c
+## Conduct an investigation into the missing data.
+
+## Provide an appropriate visualisation of the missing data.
+
+likert.histogram.plot(results) # missing data
+
+## Test whether or not the data is missing completely at random.
+
+## Impute missing data using regression imputation and predictive mean matching.
+
+## Use appropriate plots to assess the two imputations for the variable with the
+## most missing data. Comment on the results.
+
+## Use Kolmogorov-Smirnov tests to assess the imputations for the variable with
+## the most missing data. Comment on the results.
+
+## Select an imputation method, with justification, and save the dataset that is
+## created when this imputation method is used.
+
+
